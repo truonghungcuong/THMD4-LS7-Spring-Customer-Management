2e04ea4fab2d83ae99f181baf54edc0705c6ca37
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-03-30 15:03:05 +0700

    Spring-Customer-Management

---
+++ src/main/java/com/codegym/service/customer/CustomerService.java
@@ -0,0 +1,41 @@
+package com.codegym.service.customer;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import com.codegym.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService implements ICustomerService {
+
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Override
+    public Iterable findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+}
